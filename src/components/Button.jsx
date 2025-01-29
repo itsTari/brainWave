24999f6b2fe105441be4ccddcb4e53f86c1b8b16
itsTari <tariebisamuel16@gmail.com>
2025-01-29 18:59:41 +0100

    built the header component

---
+++ src/components/Button.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ButtonSvg from "../assets/svg/ButtonSvg";
+
+const Button = ({ className, href, onClick, children, px, color }) => {
+    const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-7'} ${color ? 'text-n-8' :'text-n-1'} ${className || ''}`
+    const spanClasses = 'relative z-10'
+  const renderBtn = () => (
+
+    <button className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(color)}
+    </button>
+  );
+
+  const renderlink = () =>(
+    <a href={href} className={classes}>
+        <span className={spanClasses}>{children}</span>
+        {ButtonSvg(color)}
+    </a>
+  )
+
+  return href ? renderlink() : renderBtn();
+};
+
+export default Button;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import {brainwave} from '../assets'
+import {navigation} from '../constant'
+
+
+const Header = () => {
+  return (
+    <div className='fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm '>
+        <div className='flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4'>
+            <a href='#hero' className='block w-[12rem] xl:mr-8 '>
+                <img src={brainwave} alt='brainwave' width={190} height={40}/>
+            </a>
+            <nav className='hidden fixed top-[5rem] right-0 left-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent'>
+                <div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'>
+                    {navigation.map((list) =>(
+                        <a key={list.id} href={list.url} className={`block font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 px-6 py-6 md:py-8  lg:-mr-0.25 lg:text-xs lg:font-semibold ${list.onlyMobile ? 'lg:hidden':''}`}>{list.title}</a>
+                    ))}
+                </div>
+            </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Header
