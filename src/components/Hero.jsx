0ab95248256a0375847bb64cb4bc6511b756ad8a
itsTari <tariebisamuel16@gmail.com>
2025-01-31 14:42:57 +0100

    hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,59 @@
+import React, { useRef } from 'react'
+import Section from './Section'
+import { curve, heroBackground, robot } from '../assets'
+import Button from './Button'
+import { BackgroundCircles, Gradient, BottomLine } from './design/Hero'
+import { heroIcons } from '../constant'
+import { ScrollParallax } from 'react-just-parallax'
+
+
+const Hero = () => {
+    const parallaxRef = useRef(null)
+  return (
+    
+    <Section className='pt-[12rem] -mt-[5.25rem]' crosses crossesOffset='lg:translate-y-[5.25rem]' customPadding id='hero'>
+        <div className='container relative' ref={parallaxRef}>
+            <div className='relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem] '>
+                <h1 className='h1 mb-6'>
+                    Explore the Possibilities of&nbsp;AI&nbsp;chatting with {''} 
+                    <span className='inline-block relative'>BrainWave <img className='absolute top-full left-0 w-full xl:-mt-2' width={624} height={28} src={curve} alt='curve'/></span>
+                </h1>
+                <p className='body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8'>
+                    Unleash the power of AI within BrainWave. Upgrade your Productivity with BrainWave, the open AI chat app.
+                </p>
+                <Button href='/pricing' color>
+                        get started
+                </Button>
+            </div>
+            <div className='relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-22'>
+                <div className='relative z-1 p-0.5 rounded-2xl bg-conic-gradient '>
+                    <div className='relative bg-n-8 rounded-[1rem]'>
+                        <div className='h-[1.4rem] bg-n-10 rounded-t-[0.8rem]  '/>
+                        <div className='aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]'>
+                            <img src={robot} className='w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]' alt='robot'  width={1024} height={490}/>
+                            <ScrollParallax isAbsolutelyPositioned>
+                                <ul className='hidden xl:flex absolute -left-[5.5rem] p-1 bottom-[7.5rem] bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl '>
+                                    {heroIcons.map((icon, id)=>(
+                                        <li key={id} className='p-5'>
+                                            <img src={icon} alt='icon' width={24} height={25}/>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </ScrollParallax>
+                        </div>
+                    </div>
+                    <Gradient/>
+                </div>
+                <div className='absolute -top-[50%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]'>
+                    <img src={heroBackground} alt='heroBackground' className='w-full' width={1440} height={1800}/>
+                </div>
+                <BackgroundCircles/>
+            </div>
+        </div>
+        <BottomLine/>
+    </Section>
+    
+  )
+}
+
+export default Hero
