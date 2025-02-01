337a920a9a11b2a6621085a34a48ad930a9754c5
itsTari <tariebisamuel16@gmail.com>
2025-02-01 16:41:42 +0100

    built the benefit component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import ButtonGradient from './assets/svg/ButtonGradient'
 // import Button from './components/Button';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Value from './components/Benefits';
 function App() {
   return (
     <>
     <div className='pt-[4.7rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header/>
         <Hero/>
+        <Value/>
     </div>
       <ButtonGradient/>
     </>
