74faba0ecec245973074eab52dc4611eb9834a82
itsTari <tariebisamuel16@gmail.com>
2025-02-04 13:44:56 +0100

    wrap the service component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,9 @@
 import Section from "./Section"
 import Heading from './Heading'
-import { check, service1 } from "../assets"
-import { brainwaveServices } from "../constant"
+import { check, service1, service2, service3 } from "../assets"
+import { brainwaveServices, brainwaveServicesIcons } from "../constant"
+import Generating from "./Generating"
+import { Gradient, PhotoChatMessage, VideoBar, VideoChatMessage } from "./design/Services"
 
 const Services = () => {
   return (
@@ -25,7 +27,43 @@ const Services = () => {
                 ))}
               </ul>
             </div>
+            <Generating className='absolute bottom-4 left-4 right-4 border border-n-1/10 md:left-1/2  md:bottom-8 md:-translate-x-1/2'/>
           </div>
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+                {/* for the first grid element */}
+                <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+                  <div className="absolute inset-0">
+                    <img src={service2} className="h-full w-full object-cover" width={630} height={750} alt="robot"/>
+                  </div>
+                  <PhotoChatMessage/>
+                  <div className="absolute flex flex-col inset-0 justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                    <h4 className="h4 mb-3">Photo editing</h4>
+                    <p className="body-2 mb-[1rem] text-n-3">Automatically enhance your photos using our AI app&apos;s photo editing feature. Try it now! </p>
+                  </div>
+                </div>
+                {/* for the second grid element */}
+                <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+                    <div className="py-12 px-4 xl:px-8">
+                        <h4 className="h4 mb-4">Video generation</h4>
+                        <p className="body-2 mb-[2rem] text-n-3">The world&apos;s most powerful AI photo and video art generation engine. What will you create?</p>
+                        <ul className="flex items-center justify-between">
+                          {brainwaveServicesIcons.map((item, id) =>(
+                            <li key={id} className={`flex items-center justify-center rounded-2xl ${id ===2? 'w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]': 'flex w-10 h-10 bg-n-6 md:w-15 md:h-15'}`}>
+                              <div className={`${id === 2? 'bg-n-7 w-full h-full flex items-center justify-center rounded-2xl':''}`}>
+                                <img src={item} alt={item} width={24} height={24}/>
+                              </div>
+                            </li>
+                          ))}
+                        </ul>
+                    </div>
+                    <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
+                      <img src={service3} alt="robot" className="w-full h-full object-cover" width={520} height={400}/>
+                      <VideoChatMessage/>
+                      <VideoBar/>
+                    </div>
+                </div>
+          </div>
+          <Gradient/>
         </div>
       </div>
     </Section>
